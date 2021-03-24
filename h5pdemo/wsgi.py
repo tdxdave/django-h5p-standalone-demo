ea4af6a1f2c3500bb0dceb700dd980938b51ee45
Dave Bauer <dave.bauer@gmail.com>
2021-03-23 21:03:07 -0400

    Use wsgi.py

--- h5pdemo/wsgi.py
+++ h5pdemo/wsgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for h5pdemo project.
+WSGI config for h5pdemo project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h5pdemo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h5pdemo.settings")
 
-application = get_asgi_application()
+application = get_wsgi_application()
